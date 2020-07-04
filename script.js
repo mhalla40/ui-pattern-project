84d4a95c01df4fb243f03e627739c32a38291661
Michael Hall <mhalla40@gmail.com>
2020-07-04 08:07:05 -0400

    bio in script

---
+++ script.js
@@ -0,0 +1,77 @@
+var characters = [
+  {
+    name: "Rick Sanchez",
+    species: "Human",
+    status: "Alive",
+  },
+  {
+    name: "Morty Smith",
+    species: "Human",
+    status: "Alive",
+  },
+  {
+    name: "Summer Smith",
+    species: "Human",
+    status: "Alive",
+  },
+  {
+    name: "Beth Smith",
+    species: "Human",
+    status: "Alive",
+  },
+  {
+    name: "Jerry Smith",
+    species: "Human",
+    status: "Alive",
+  },
+  {
+    name: "Abadango Cluster Princess",
+    species: "Alien",
+    status: "Alive",
+  },
+  {
+    name: "Abradolf Lincler",
+    species: "Human",
+    status: "unknown",
+  },
+  {
+    name: "Adjudicator Rick",
+    species: "Human",
+    status: "Dead",
+  },
+  {
+    name: "Agency Director",
+    species: "Human",
+    status: "Dead",
+  },
+  {
+    name: "Alan Rails",
+    species: "Human",
+    status: "Dead",
+  },
+  {
+    name: "Albert Einstein",
+    species: "Human",
+    status: "Dead",
+  },
+  {
+    name: "Alexander",
+    species: "Human",
+    status: "Dead",
+  },
+  {
+    name: "Alien Googah",
+    species: "Alien",
+    status: "unknown",
+  },
+  {
+    name: "Alien Morty",
+    species: "Alien",
+    status: "unknown",
+  },
+  {
+    name: "Alien Rick",
+    species: "Alien",
+    status: "unknown",
+  },
+];
